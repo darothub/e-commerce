39104b9e699178bfb99592e6561a5fdf0d671f11
darothub <darotudeen@gmail.com>
2019-10-24 14:08:45 +0100

    add keyframe to loading

--- commerce/src/Nav/Nav.js
+++ commerce/src/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './Nav.css'
 import LeftNav from './LeftNav'
@@ -6,14 +6,28 @@ import RightNav from './Rightnav'
 
 
 function Nav(){
+  let[loading, setLoading] = useState(true)
+    useEffect(()=>{
+      setTimeout(()=>{
+        setLoading(loading = false)
+      }, 1500)
+    
+    }, [])
     return (
       <>
-        <div className="container-fluid wrapper d-flex">
-          <div className="container d-flex p-2">
-            <LeftNav />
-            <RightNav />
+        {loading ? (
+          <button class="btn btn-primary" disabled>
+            <span class="spinner-grow spinner-grow-sm"></span>
+            Loading..
+          </button>
+        ) : (
+          <div className="container-fluid wrapper d-flex">
+            <div className="container d-flex p-2">
+              <LeftNav />
+              <RightNav />
+            </div>
           </div>
-        </div>
+        )}
       </>
     );
 
